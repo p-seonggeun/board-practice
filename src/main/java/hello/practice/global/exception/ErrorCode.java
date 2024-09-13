d0871a4eb0205294e671611b8008220543427427
psg <1999byh@naver.com>
2024-09-13 15:25:44 +0900

    Feat: "예외 처리 기능"
    예외 처리 핸들러 추가
    에러코드 정의

---
+++ src/main/java/hello/practice/global/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package hello.practice.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    ALREADY_EXISTS_USERNAME(400, "ACCOUNT-001", "이미 존재하는 아이디일 경우"),
+    ALREADY_EXISTS_EMAIL(400, "ACCOUNT-002", "이미 존재하는 이메일일 경우"),
+    ALREADY_EXISTS_NICKNAME(400, "ACCOUNT-003", "이미 존재하는 닉네임일 경우");
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    ErrorCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+
+}

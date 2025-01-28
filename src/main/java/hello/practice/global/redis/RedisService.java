a958bb1dc736a2336e2897a1ae208a3b9ed3d7cf
psg <1999byh@naver.com>
2025-01-28 11:32:25 +0900

    refactor: redis key 네이밍 규칙 설정

--- src/main/java/hello/practice/global/redis/RedisService.java
+++ src/main/java/hello/practice/global/redis/RedisService.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -13,24 +15,28 @@ public class RedisService {
     private final RedisTemplate<String, Object> redisTemplate;
 
     public void saveRefreshToken(String username, String refreshToken, Long timeout) {
-        redisTemplate.opsForValue().set(username, refreshToken, timeout);
-        log.info("{}의 리프레시 토큰 저장", username);
+        String key = "refreshToken:user:" + username;
+        redisTemplate.opsForValue().set(key, refreshToken, timeout, TimeUnit.MILLISECONDS);
+        log.info("{}의 리프레시 토큰 저장", key);
     }
 
     public String getRefreshToken(String username) {
-        String result = (String) redisTemplate.opsForValue().get(username);
-        log.info("{}의 리프레시 토큰: {}", username, result);
+        String key = "refreshToken:user:" + username;
+        String result = (String) redisTemplate.opsForValue().get(key);
+        log.info("{}의 리프레시 토큰: {}", key, result);
         return result;
     }
 
     public void deleteRefreshToken(String username) {
-        Boolean result = redisTemplate.delete(username);
-        log.info("{}의 리프레시 토큰 삭제 결과: {}", username, result);
+        String key = "refreshToken:user:" + username;
+        Boolean result = redisTemplate.delete(key);
+        log.info("{}의 리프레시 토큰 삭제 결과: {}", key, result);
     }
 
     public boolean existRefreshToken(String username) {
-        Boolean result = redisTemplate.hasKey(username);
-        log.info("{}의 리프레시 토큰 존재 확인 결과: {}", username, result);
+        String key = "refreshToken:user:" + username;
+        Boolean result = redisTemplate.hasKey(key);
+        log.info("{}의 리프레시 토큰 존재 확인 결과: {}", key, result);
         return result;
     }
 }
